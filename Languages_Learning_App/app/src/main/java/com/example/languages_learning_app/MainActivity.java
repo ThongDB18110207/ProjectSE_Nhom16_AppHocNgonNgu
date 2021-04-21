014c55902510f85192f673b9a3bbb44b10f81526
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2021-04-21 17:02:21 +0700

    Done Task01
    Đăng nhập, đăng ký, xác nhận tài khoản, đổi mật khẩu

---
+++ Languages_Learning_App/app/src/main/java/com/example/languages_learning_app/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.languages_learning_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.BreakIterator;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private  TextView register, fogotPassword;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById((R.id.tvRegister));
+        register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.btLogin);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.etEmail);
+        editTextPassword = (EditText) findViewById(R.id.etPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        fogotPassword = (TextView) findViewById(R.id.tvForgotPassword);
+        fogotPassword.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvRegister:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+            case R.id.btLogin:
+                userLogin();
+                break;
+            case R.id.tvForgotPassword:
+                startActivity(new Intent(this, FogotPassword.class));
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid Email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 8){
+            editTextPassword.setError("Min password lenght is 8 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // Validate Email
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if(user.isEmailVerified()){
+                        // redirect to user profile
+                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    }
+                    else{
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this, "Check your Email to verify your account", Toast.LENGTH_LONG).show();
+                    }
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Failed to Login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
---
+++ Languages_Learning_App/app/src/main/java/com/example/languages_learning_app/User.java
@@ -0,0 +1,14 @@
+package com.example.languages_learning_app;
+
+public class User {
+    public String fullName, phone, email;
+
+    public User(){}
+
+    public User(String fullName, String phone, String email){
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+}
+
